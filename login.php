7670a9190ee6dbbdf302cfb703c7549ce95626ec
Md. Rafiur Rahman <95489408+rr-uchchash360@users.noreply.github.com>
2022-10-25 12:05:25 +0600

    Login Updated

--- login.php
+++ login.php
@@ -21,7 +21,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="Login.css">
+    <link rel="stylesheet" href="login.css">
+    <link rel="icon" href="icons/shop.png" type="image/icon type">
 </head>
 <body>
 
@@ -30,43 +31,38 @@
         <div class="side-frame">
 
             <div class="logo">
-                
+                <img src="icons/shop.png" style="width: 280px; height: 280px;">                
             </div>
     
             <div class="system-name">
-                <label>Halkhata - Shop Management System</label>
+                <label>Halkhata<br>Shop Management System</label>
             </div>
     
             <div class="motto">
-                <label>Less Hassle, More Profit.</label>
+                <label>Less Hassle, More Profit!</label>
             </div>
     
         </div>
 
         <div class="wrapper">
-            <form action='login.php' method="get">
+            <form action='' method="get">
                 <div class="welcome-label">
-                    <label>Welcome to Halkhata - The Shop Management System</label>
+                    <label>Welcome to Halkhata!</label>
                 </div>
                 <div class="login-label">
                     <label>Please Login!</label>
                 </div>
-                <div class="shop-id-label">
-                    <label>Shop ID</label>
-                </div>                     
+                                   
                 <div>
-                    <input type="text" name ="shopID" placeholder="Enter the Shop ID" class="shop-id-input">
+                    <input type="number" onkeyup="return event.charCode >= 48" min="1" class="shop-id-input" placeholder="Enter the Shop ID">
                 </div>
 
-
-                <div class="password-label">
-                    <label>Password</label>
-                </div>        
                 <div>
-                    <input type="text" name ="password" placeholder="Enter Your Password"  class="password-input">
+                    <input type="password" placeholder="Enter Your Password"  class="password-input">
                 </div>
+
                 <div>
-                    <button type="submit" name = "logIn" class="login-button" required>Login</button>
+                    <button type="submit" class="login-button" required>Login</button>
                 </div>
             </form>
         </div>
