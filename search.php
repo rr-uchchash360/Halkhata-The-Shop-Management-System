c3bf9080f1197e468dbb032e082b9fe6ad5eae7a
zubayertahmid <95120572+zubayertahmid@users.noreply.github.com>
2022-10-30 15:40:25 +0600

    Created search.php
    We have started implementing the search system for the products.

---
+++ search.php
@@ -0,0 +1,91 @@
+<?php
+    $hostname = 'localhost';
+    $username = 'root';
+    $password = '';
+    $dbname = 'users';
+    $conn = mysqli_connect($hostname, $username, $password, $dbname);
+?>
+<!DOCTYPE html>
+<html lang="en">
+  
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Search</title>
+    <link rel="stylesheet" href="search.css">
+    <link rel="icon" href="icons/search.png" type="image/icon type">
+</head>
+  
+<body>
+
+    <div class="navbar">
+
+        <div class="navbar-shop-logo">
+            <a href="add.html"><img src="icons/shop.png" style="width: 90px; height: 90px;">
+        </div>
+
+        <div class="navbar-add-product-button">
+            <a href="add.html">Add</a>
+        </div>
+        <div class="navbar-sell-product-button">
+            <a href="sell.html">Sell</a>
+        </div>
+        <div class="navbar-return-product-button">
+            <a href="return.html">Return</a>
+        </div>
+
+        <div class="navbar-dashboard-button">
+            <a href = login.html>Dashboard</a>
+        </div>
+
+        <div class="navbar-logout-button">
+            <a href = login.html>Log Out</a>
+        </div>
+
+    </div>
+
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="GET" class="wrapper">
+        <div class="wrapper-search-product-label">
+            <img src="icons/search.png" style="width: 22px;height: 22px;">
+            <label>Search Product</label>
+        </div>
+        <div class="wrapper-product-id-label">
+            <label>Product ID</label>
+        </div>
+        <div>
+            <input type="text" class="wrapper-product-id-input" name="productID" placeholder="Enter Product ID">
+        </div> 
+        <div>
+            <button type="submit" class="wrapper-search-button" required>Search</button>
+        </div>
+       
+    </form>
+    
+    <?php 
+        if(isset($_GET['productID'])){
+           $product_id = $_GET['productID'];
+            $sql = "SELECT * FROM products WHERE product_id = '$product_id'";
+            echo "<table border='1'>
+                    <thead>
+                        <tr>Product ID</tr>
+                        <tr>Product Name</tr>
+                        <tr>Product Quantity</tr>
+                        <tr>Product Price</tr>
+                    </thead>
+                </table>" ;
+
+        }
+    ?>
+    
+    
+    <div class="footer">
+        <label>©</label>
+        <a href = https://github.com/rr-uchchash360>Md. Rafiur Rahman</a>
+        <a href = https://github.com/zubayertahmid>Zubayer Tahmid</a>
+        <a href = https://github.com/sadman89>Sadman Sadiq</a>
+    </div>
+
+</body>
+  
+</html>
