8f129b96738b329d024c5c3b4db50733a3b35fab
zubayertahmid <95120572+zubayertahmid@users.noreply.github.com>
2022-10-30 14:06:19 +0600

    add.php and login.php was added
    We were facing some issues regarding the merging of files. It was resolved in this commit.

---
+++ add.php
@@ -0,0 +1,100 @@
+<?php
+    $hostname = 'localhost';
+    $username = 'root';
+    $password = '';
+    $dbname = 'users';
+    $conn = mysqli_connect($hostname, $username, $password, $dbname);
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+  
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Product</title>
+    <link rel="stylesheet" href="add.css">
+    <link rel="icon" href="icons/add.png" type="image/icon type">
+</head>
+
+<body>
+<?php 
+        if(isset($_POST['product_id'])){
+            $product_id = $_POST['product_id'];
+            $product_name = $_POST['product_name'];
+            $product_quantity = $_POST['product_quantity'];
+            $product_price = $_POST['product_price'];
+            
+            $sql = "INSERT INTO products (product_name, product_quantity, product_id, product_price) 
+                    VALUES ('$product_name', '$product_quantity', '$product_id', '$product_price')";
+                if($conn->query($sql) == "TRUE"){
+                echo '<script>alert("Data Inserted Successfully!")</script>';
+            }
+        }
+    ?>
+    <div class="navbar">
+
+        <div class="navbar-shop-logo">
+            <a href="add.html"><img src="icons/shop.png" style="width: 90px; height: 90px;">
+        </div>
+
+        <div class="navbar-search-product-button">
+            <a href="search.html">Search</a>
+        </div>
+        <div class="navbar-sell-product-button">
+            <a href="sell.html">Sell</a>
+        </div>
+        <div class="navbar-return-product-button">
+            <a href="return.html">Return</a>
+        </div>
+
+        <div class="navbar-dashboard-button">
+            <a href = login.html>Dashboard</a>
+        </div>
+
+        <div class="navbar-logout-button">
+            <a href = login.html>Log Out</a>
+        </div>
+
+    </div>
+
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" class="wrapper">
+    
+        <div class="wrapper-add-product-label">
+            <img src="icons/add.png" style="width: 22px;height: 22px;">
+            <label>Add Product</label>
+        </div>
+
+        <div>
+            <input type="text" onkeyup="return event.charCode >= 48" name="product_id" min="1" class="wrapper-product-id-input" placeholder="Enter Product ID">
+        </div>
+        
+        <div>
+            <input type="text" class="wrapper-product-name-intput" name="product_name" placeholder="Enter Product Name">
+        </div>
+        
+        <div>
+            <input type="number" onkeyup="return event.charCode >= 48" name="product_quantity" min="1" class="wrapper-product-quantity-intput" placeholder="Enter Product Quantity">
+        </div>
+
+        <div>
+            <input type="number" onkeyup="return event.charCode >= 48" min="1" name="product_price" class="wrapper-product-price-intput" placeholder="Enter Product Price">
+        </div>
+
+        <div>
+            <button type="submit" class="wrapper-add-product-button" required>Add Product</button>
+        </div>
+        
+    </form>
+
+    <div class="footer">
+        <label>©</label>
+        <a href = https://github.com/rr-uchchash360>Md. Rafiur Rahman</a>
+        <a href = https://github.com/zubayertahmid>Zubayer Tahmid</a>
+        <a href = https://github.com/sadman89>Sadman Sadiq</a>
+    </div>
+
+</body>
+  
+</html>
---
+++ login.php
@@ -0,0 +1,75 @@
+<?php
+    $hostname = 'localhost';
+    $username = 'root';
+    $password = '';
+    $dbname = 'users';
+    $conn = mysqli_connect($hostname, $username, $password, $dbname);
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="login.css">
+    <link rel="icon" href="icons/shop.png" type="image/icon type">
+</head>
+<body>
+<?php 
+        if(isset($_POST['user_id'])){
+            $user_id = $_POST['user_id'];
+            $user_password = $_POST['user_password'];
+            $sql = "SELECT * FROM users WHERE user_id = '$user_id' AND user_password = '$user_password'";
+            $query = $conn->query($sql);
+
+            if(mysqli_num_rows($query) > 0){
+                echo '<script>alert("You have logged In Successfully!")</script>';
+            }
+        }
+    ?>
+    <div class="page">
+
+        <div class="side-frame">
+
+            <div class="logo">
+                <img src="icons/shop.png" style="width: 280px; height: 280px;">                
+            </div>
+    
+            <div class="system-name">
+                <label>Halkhata<br>Shop Management System</label>
+            </div>
+    
+            <div class="motto">
+                <label>Less Hassle, More Profit!</label>
+            </div>
+    
+        </div>
+
+        <div class="wrapper">
+            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+                <div class="welcome-label">
+                    <label>Welcome to Halkhata!</label>
+                </div>
+                <div class="login-label">
+                    <label>Please Login!</label>
+                </div>
+                                   
+                <div>
+                    <input type="text" onkeyup="return event.charCode >= 48" min="1" class="shop-id-input" name="user_id" placeholder="Enter the Shop ID">
+                </div>
+
+                <div>
+                    <input type="password" placeholder="Enter Your Password" name ="user_password" class="password-input">
+                </div>
+
+                <div>
+                    <button type="submit" class="login-button" required>Login</button>
+                </div>
+            </form>
+        </div>
+
+    </div> 
+</body>
+</html>
