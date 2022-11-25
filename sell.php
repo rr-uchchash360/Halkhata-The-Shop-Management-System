ff9da2607e705826b3ff728f2bcfe912930e44e3
Md. Rafiur Rahman <95489408+rr-uchchash360@users.noreply.github.com>
2022-11-25 12:53:07 +0600

    Sell page updated

--- sell.php
+++ sell.php
@@ -38,8 +38,8 @@
 <div class="navbar-add-product-button">
     <a href='add.php'><button>Add</button></a>
 </div>
-<div class="navbar-search-product-button">
-    <a href='search.php'><button>Search</button></a>
+<div class="navbar-update-product-button">
+    <a href='update.html'><button>Update</button></a>
 </div>
 <div class="navbar-return-product-button">
     <a href='return.php'><button>Return</button></a>
