1ae31748b06db6d5c4fd5393d52027ea243a4923
Md. Rafiur Rahman <95489408+rr-uchchash360@users.noreply.github.com>
2022-12-19 22:10:35 +0600

    Frontend for Products page

---
+++ product.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
+
+    <title>Product</title>
+  </head>
+  <body>
+    <div class="container">
+        <button class="add-user-button">Add Product</button>
+
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Serial Number</th>
+              <th scope="col">Product ID</th>
+              <th scope="col">Product Name</th>
+              <th scope="col">Buying Price</th>
+              <th scope="col">Selling Price</th>
+              <th scope="col">Availability</th>
+            </tr>
+          </thead>
+          <tbody>
+            
+
+
+            <!-- Backend Code -->
+            
+
+
+          </tbody>
+        </table>
+
+    </div>
+
+    
+
+  
+  </body>
+</html>
